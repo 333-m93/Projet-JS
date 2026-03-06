743f65747d65931f831cfab264e70f4cd39be7d6
Blanka 3719 <mmohammed>
2026-03-06 15:32:10 +0100

    Debut du jeu

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("html")))
+	fileServer := http.FileServer(http.Dir("."))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "html/index.html")
+			return
+		}
+		fileServer.ServeHTTP(w, r)
+	})
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
