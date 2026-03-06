81dd9af698d64f8037192405a736af02b38f40a1
Blanka 3719 <mmohammed>
2026-03-06 15:19:40 +0100

    Html , Server

--- server/server.go
+++ server/server.go
@@ -6,9 +6,7 @@ import (
 )
 
 func StartServer(port string) error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../test.html")
-	})
+	http.Handle("/", http.FileServer(http.Dir("../html")))
 
 	log.Printf("Serveur lancé sur http://localhost:%s", port)
 	return http.ListenAndServe(":"+port, nil)
