8c433b32df6c8a73495145d51e99d1cd5d982dc6
Blanka 3719 <mmohammed>
2026-03-06 15:32:10 +0100

    Debut du jeu

--- server/server.go
+++ server/server.go
@@ -3,10 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func StartServer(port string) error {
-	http.Handle("/", http.FileServer(http.Dir("../html")))
+	rootDir := filepath.Join("..")
+	fileServer := http.FileServer(http.Dir(rootDir))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, filepath.Join(rootDir, "html", "index.html"))
+			return
+		}
+		fileServer.ServeHTTP(w, r)
+	})
 
 	log.Printf("Serveur lancé sur http://localhost:%s", port)
 	return http.ListenAndServe(":"+port, nil)
