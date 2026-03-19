78c2821547369d80c0f8738a0f0213604165ba21
Blanka 3719 <mmohammed>
2026-03-19 16:56:28 +0100

    Main.go

--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("."))
+	fs := http.FileServer(http.Dir("."))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
 			http.ServeFile(w, r, "html/index.html")
 			return
 		}
-		fileServer.ServeHTTP(w, r)
+		fs.ServeHTTP(w, r)
 	})
-
 	log.Println("Serveur lancé sur http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
