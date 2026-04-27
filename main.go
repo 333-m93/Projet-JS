5d4a3142dc1e192df77d618a8b2aa40a8c3f3ea1
Blanka 3719 <mmohammed>
2026-04-27 16:42:10 +0200

    main.go

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
+import "log"
+import "net/http"
 
 func main() {
 	fs := http.FileServer(http.Dir("."))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.URL.Path {
-		case "/", "/index.html":
-			http.ServeFile(w, r, "html/index.html")
-		case "/rules", "/rules.html":
-			http.ServeFile(w, r, "html/rules.html")
-		case "/game", "/game.html":
-			http.ServeFile(w, r, "html/game.html")
-		default:
-			fs.ServeHTTP(w, r)
-		}
+		if p := map[string]string{"/": "index", "/index.html": "index", "/rules": "rules", "/rules.html": "rules", "/game": "game", "/game.html": "game"}[r.URL.Path]; p != "" { http.ServeFile(w, r, "html/"+p+".html"); return }
+		fs.ServeHTTP(w, r)
 	})
-	log.Println("Serveur lancé sur http://localhost:8080")
+	log.Println("Serveur lance sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
